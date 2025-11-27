c0dbaa40d7a24555b4b79523d67d5b568d41edbd
ADAIBLOG <186315423+ADAIBLOG@users.noreply.github.com>
2025-11-28 04:02:09 +0800

    Update aboutPage.dart

--- budget/lib/pages/aboutPage.dart
+++ budget/lib/pages/aboutPage.dart
@@ -6,10 +6,6 @@ import 'package:budget/widgets/settingsContainers.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:budget/widgets/openBottomSheet.dart';
-import 'package:budget/widgets/framework/popupFramework.dart';
-import 'package:budget/appState/appState.dart';
-import 'package:budget/utilities/deviceOrientations.dart';
 
 class AboutPage extends StatefulWidget {
   const AboutPage({Key? key}) : super(key: key);
@@ -19,45 +15,14 @@ class AboutPage extends StatefulWidget {
 }
 
 class _AboutPageState extends State<AboutPage> {
-  GlobalKey<PageFrameworkState> pageState = GlobalKey();
-
-  void scrollToTop() {
-    pageState.currentState?.scrollToTop();
-  }
-
   @override
   Widget build(BuildContext context) {
     String pageId = "About";
 
     return PageFramework(
-      key: pageState,
       listID: pageId,
       dragDownToDismiss: true,
       title: "about",
-      actions: [
-        CustomPopupMenuButton(
-          showButtons: enableDoubleColumn(context),
-          keepOutFirst: true,
-          items: [
-            DropdownItemMenu(
-              id: "settings",
-              label: "settings",
-              icon: appStateSettings["outlinedIcons"]
-                  ? Icons.more_vert_outlined
-                  : Icons.more_vert_rounded,
-              action: () {
-                openBottomSheet(
-                  context,
-                  PopupFramework(
-                    hasPadding: false,
-                    child: AboutSettings(),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
-      ],
       slivers: [
         SliverToBoxAdapter(
           child: Padding(
@@ -115,71 +80,4 @@ class _AboutPageState extends State<AboutPage> {
       ],
     );
   }
-}
-
-class AboutSettings extends StatelessWidget {
-  const AboutSettings({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SettingsContainer(
-          title: "about".tr(),
-          description: "version-1-0-4".tr(),
-          icon: appStateSettings["outlinedIcons"]
-              ? Icons.info_outlined
-              : Icons.info_rounded,
-          showBorder: true,
-        ),
-      ],
-    );
-  }
-}
-
-class CustomPopupMenuButton extends StatelessWidget {
-  final List<DropdownItemMenu> items;
-  final bool showButtons;
-  final bool keepOutFirst;
-
-  const CustomPopupMenuButton({
-    Key? key,
-    required this.items,
-    required this.showButtons,
-    required this.keepOutFirst,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return PopupMenuButton(
-      itemBuilder: (context) => items.map((item) {
-        return PopupMenuItem(
-          onTap: item.action,
-          child: ListTile(
-            leading: Icon(item.icon),
-            title: Text(item.label),
-          ),
-        );
-      }).toList(),
-      icon: Icon(
-        appStateSettings["outlinedIcons"]
-            ? Icons.more_vert_outlined
-            : Icons.more_vert_rounded,
-      ),
-    );
-  }
-}
-
-class DropdownItemMenu {
-  final String id;
-  final String label;
-  final IconData icon;
-  final Function() action;
-
-  DropdownItemMenu({
-    required this.id,
-    required this.label,
-    required this.icon,
-    required this.action,
-  });
 }
