7bd8107a108b0df2ea501c95f2fc041d088b3ea6
ADAIBLOG <186315423+ADAIBLOG@users.noreply.github.com>
2025-11-27 17:02:25 +0800

    Update functions.dart

--- budget/lib/functions.dart
+++ budget/lib/functions.dart
@@ -74,13 +74,13 @@ extension DateUtils on DateTime {
   DateTime lastDayOfMonth() {
     return DateTime(this.year, this.month + 1, 0);
   }
-
+  
   DateTime startOfDay() {
-    return DateTime(this.year, this.month, this.day, 0, 0, 0);
+    return DateTime(this.year, this.month, this.day, 0, 0, 0, 0, 0);
   }
-
+  
   DateTime endOfDay() {
-    return DateTime(this.year, this.month, this.day, 23, 59, 59);
+    return DateTime(this.year, this.month, this.day, 23, 59, 59, 999, 999);
   }
 }
 
