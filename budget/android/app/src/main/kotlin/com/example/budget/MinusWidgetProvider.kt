7d9d4b79072eb236642bd0196edfa120f730a953
ADAIBLOG <186315423+ADAIBLOG@users.noreply.github.com>
2025-12-01 18:19:15 +0800

    Update MinusWidgetProvider.kt

--- budget/android/app/src/main/kotlin/com/example/budget/MinusWidgetProvider.kt
+++ budget/android/app/src/main/kotlin/com/example/budget/MinusWidgetProvider.kt
@@ -3,8 +3,11 @@ package com.budget.tracker_app
 import android.appwidget.AppWidgetManager
 import android.content.Context
 import android.content.SharedPreferences
+import android.graphics.BitmapFactory
 import android.net.Uri
+import android.view.View
 import android.widget.RemoteViews
+import es.antonborri.home_widget.HomeWidgetBackgroundIntent
 import es.antonborri.home_widget.HomeWidgetLaunchIntent
 import es.antonborri.home_widget.HomeWidgetProvider
 
