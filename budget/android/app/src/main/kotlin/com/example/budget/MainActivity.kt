bad66f00372f41a976e4123d7669dd97d3f5afac
ADAIBLOG <186315423+ADAIBLOG@users.noreply.github.com>
2025-12-18 03:04:46 +0800

    Update MainActivity.kt

--- budget/android/app/src/main/kotlin/com/example/budget/MainActivity.kt
+++ budget/android/app/src/main/kotlin/com/example/budget/MainActivity.kt
@@ -1,6 +1,18 @@
 package com.budget.tracker_app
 
 import io.flutter.embedding.android.FlutterFragmentActivity
+import androidx.activity.OnBackPressedCallback
 
 class MainActivity: FlutterFragmentActivity() {
+    override fun onCreate(savedInstanceState: android.os.Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Handle back press to move task to back
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // Instead of finishing the activity, move the app to background
+                moveTaskToBack(true)
+            }
+        })
+    }
 }
