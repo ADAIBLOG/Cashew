1a144d5d99dc747068c38cf812c331ffd69d710a
ADAIBLOG <186315423+ADAIBLOG@users.noreply.github.com>
2026-02-07 01:30:32 +0800

    Update autoTransactionsPageEmail.dart

--- budget/lib/pages/autoTransactionsPageEmail.dart
+++ budget/lib/pages/autoTransactionsPageEmail.dart
@@ -92,29 +92,13 @@ class InitializeNotificationService extends StatefulWidget {
 }
 
 class _InitializeNotificationServiceState
-    extends State<InitializeNotificationService> with WidgetsBindingObserver {
+    extends State<InitializeNotificationService> {
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addObserver(this);
     Future.delayed(Duration.zero, () async {
       initNotificationScanning();
     });
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed) {
-      Future.delayed(Duration.zero, () async {
-        initNotificationScanning();
-      });
-    }
   }
 
   @override
